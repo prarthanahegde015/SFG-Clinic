e1dcb117507790b72846ee1f9387b87a233bbe3c
Prarthana <phegde015@gmail.com>
2021-08-30 02:50:44 +0530

    Adding the new project contents to the repo

---
+++ src/main/java/guru/springframework/SFGClinic/SfgClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.SFGClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgClinicApplication.class, args);
+	}
+
+}
